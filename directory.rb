c269273417933bed7ac8a486891f7cad793de198
KrzysztofBalejko <ec2-user@ip-172-31-21-6.eu-west-1.compute.internal>
2018-12-14 21:53:42 +0000

    Ex7: Asking for cohort, setting default value

--- directory.rb
+++ directory.rb
@@ -1,26 +1,36 @@
 def input_students
   puts "Please enter the names of the students, hobbies, height and countries of birth"
-  puts "To finish, hit return 4 times"
+  puts "To finish, hit return 2 times"
   students = []
   name = gets.chomp()
   hobby = gets.chomp()
+  # Program will exit if return is hit twice
+  exit(0) if name == '' && hobby == ''
   country = gets.chomp()
   height = gets.chomp()
+  puts "Please enter the cohort"
+  value = gets.chomp()
+  # May is the shortes month in character length. I'm using November as a default
+  value.length < 3 ? cohort = :november : cohort = value.to_sym
 
   while !name.empty? do
-    students << {name: name, hobby: hobby, country: country, height: height, cohort: :november}
+    students << {name: name, hobby: hobby, country: country, height: height, cohort: cohort}
     puts "Now we have #{students.count} students"
     name = gets.chomp()
     hobby = gets.chomp()
+    # Loop will be broken if the return is pressed twice
+    break if name == '' && hobby == ''
     country = gets.chomp()
     height = gets.chomp()
+    puts "Please enter the cohort"
+    value = gets.chomp()
+    value.length < 3 ? cohort = :november : cohort = value.to_sym
   end
   students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "--------------------------------"
+  puts "The students of Villains Academy", "--------------------------------"
 end
 
 def print_footer(names)
@@ -35,7 +45,6 @@ count = 0
 length = students.length
 
 while count < length do
-  # Added .center method to align the output
   puts "#{hash[:name].center(20)} #{hash[:hobby].to_s.center(20)} #{hash[:country].center(20)} #{hash[:height].to_s.center(20)} (#{hash[:cohort]} cohort)"
   count += 1
   hash = students[count]
